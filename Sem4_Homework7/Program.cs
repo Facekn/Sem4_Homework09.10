2655d7827cda3d90eef68c2368468becfe685737
Ilya Grigorev <facekn110388@gmail.com>
2022-10-16 12:52:40 +0300

    Разница между max и min элементов заданного массива

---
+++ Sem4_Homework7/Program.cs
@@ -0,0 +1,23 @@
+﻿void PrintArray (double[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+    Console.Write($"{array[i]} ");
+}
+double [] array = {0.34, 8.56, 4.32, -9.47, 3.25};
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i]>max) max = array[i];
+}
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i]<min) min = array[i];
+}
+
+double result = max-min;
+Console.WriteLine("Заданный массив:");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine ($"Разница между максимальным и минимальным элементами массива {result}");

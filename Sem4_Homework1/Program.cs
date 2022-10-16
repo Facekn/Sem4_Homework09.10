497b0cd6de9c443aae8e9778c54f12b14b2e3dae
Ilya Grigorev <facekn110388@gmail.com>
2022-10-16 09:50:30 +0300

    Вывод на экран массива 01 из 8

---
+++ Sem4_Homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random(). Next(0,2);     
+    }    
+}
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(array[i]);
+    }
+}
+int[] array = new int [8];
+FillArray(array);
+PrintArray(array);

f000591a1778be82540c4c342788d56b279d287a
Ilya Grigorev <facekn110388@gmail.com>
2022-10-16 11:39:12 +0300

    Сумма чисел в массиве, стоящие на нечетной позиции

---
+++ Sem4_Homework5/Program.cs
@@ -0,0 +1,44 @@
+﻿void FillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+      Random size = new Random();
+      array [i] = size.Next(min, max);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+}
+
+Console.WriteLine("Задайте длину массива");
+int size = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите минимальное значение для массива ");
+int min = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Введите максимальное значение для массива ");
+int max = int.Parse(Console.ReadLine() ?? "0");
+
+int[] array = new int[size];
+
+FillArray( array, min, max);
+PrintArray(array);
+
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i%2==1)
+    {
+       sum = sum + array[i];
+    }
+    else
+    {
+      sum = sum+0;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях, равна  {sum}");

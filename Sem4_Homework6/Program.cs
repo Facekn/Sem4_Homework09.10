8dc3b8595cdc635c69e5bb79f7d07f39eb256e18
Ilya Grigorev <facekn110388@gmail.com>
2022-10-16 12:40:13 +0300

    Произведение парных чисел в массиве

---
+++ Sem4_Homework6/Program.cs
@@ -0,0 +1,37 @@
+﻿ void FillArray(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.Write("Задайте длину массива ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите минимальное значение для массива ");
+int min = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите максимальное значение для массива ");
+int max = int.Parse(Console.ReadLine() ?? "0");
+int [] array = new int[size];
+
+FillArray(array, min, max);
+PrintArray (array);
+Console.WriteLine(" ");
+
+int result = 0;
+for (int i = 0; i < size/2; i++)
+{ 
+    result = array[i]*array[size-1-i];
+    Console.WriteLine($"Произведение пары чисел: {result}");
+} 
+    
+
+
+

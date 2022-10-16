547392dc492f663b294cad437383999ba47571e7
Ilya Grigorev <facekn110388@gmail.com>
2022-10-16 10:45:49 +0300

    Массив из трехзначных чисел с вычислением четных и нечетных

---
+++ Sem4_Homework3/Program.cs
@@ -0,0 +1,44 @@
+﻿void FillArray(int[] array)
+{
+   int length = array.Length;
+   for (int i = 0; i < length; i++)
+   {   
+      array[i] = new Random().Next(100,999);
+   }
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+Console.Write("Введите длинну массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+
+int sumchet = 0;
+int sumnechet = 0;
+
+for (int i = 0; i < array.Length; i++)
+{    
+    if (array[i]%2==0)
+    {
+       int count = 1; 
+       sumchet = sumchet + count;
+       count++;
+    }
+    else
+    {
+       int count = 1; 
+       sumnechet = sumnechet + count;
+       count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"В массиве {sumchet} четных чисел"); 
+Console.WriteLine($"В массиве {sumnechet} нечетных чисел");
